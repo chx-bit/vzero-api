6f98e5a599ca926fbf3d86154e69ea917d8e1c22
chx-bit <135306956+chx-bit@users.noreply.github.com>
2026-03-07 01:46:24 +0700

    Update index.js

--- src/index.js
+++ src/index.js
@@ -7,6 +7,7 @@ app.use("/countries", countriesRouter);
 app.use(express.static("public"));
 app.use("/src", express.static("src"));
 
-app.listen(3000, () => {
-    console.log("running on port 3000");
-});
+app.listen(3000)
+
+export default app
+
